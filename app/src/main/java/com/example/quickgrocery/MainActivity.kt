2931286e6558da9bb47f8d69c3b8e9eecf78f770
cfsnsalazar <nsalazar0429@gmail.com>
2020-04-13 02:41:35 -0500

    implement custom toolbar and navigation drawer

--- app/src/main/java/com/example/quickgrocery/MainActivity.kt
+++ app/src/main/java/com/example/quickgrocery/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.quickgrocery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_layout.view.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    nvMenu.setNavigationItemSelectedListener(this)
+    initToolbar()
   }
+
+  private fun initToolbar() {
+    setSupportActionBar(ctAppToolbar.tbAppToolbar)
+    ctAppToolbar.tbAppToolbar.setNavigationOnClickListener {
+      dlMain.openDrawer(GravityCompat.START)
+      }
+  }
+
+  override fun onNavigationItemSelected(item: MenuItem): Boolean {
+    when(item.itemId){
+      R.id.nav_meal_schedule -> {
+
+      }
+      R.id.nav_shopping_list -> {
+
+      }
+    }
+
+    nvMenu.setCheckedItem(item.itemId)
+    dlMain.closeDrawer(GravityCompat.START)
+    return true
+  }
+
 }
---
+++ app/src/main/java/com/example/quickgrocery/views/CustomToolbar.kt
@@ -0,0 +1,14 @@
+package com.example.quickgrocery.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.example.quickgrocery.R
+
+class CustomToolbar(context: Context, attrs: AttributeSet): CoordinatorLayout(context, attrs) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.toolbar_layout, this, true)
+    }
+}
