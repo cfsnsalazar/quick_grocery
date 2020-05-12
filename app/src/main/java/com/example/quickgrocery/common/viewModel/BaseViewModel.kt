3358afb46648fed0ab531232cb71a8c50969c323
cfsnsalazar <nsalazar0429@gmail.com>
2020-05-11 21:53:52 -0500

    eliminate unneccesary wild card import

--- app/src/main/java/com/example/quickgrocery/common/viewModel/BaseViewModel.kt
+++ app/src/main/java/com/example/quickgrocery/common/viewModel/BaseViewModel.kt
@@ -2,7 +2,7 @@ package com.example.quickgrocery.common.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.*
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.quickgrocery.common.Theme
 import com.example.quickgrocery.common.ThemeDataSource
